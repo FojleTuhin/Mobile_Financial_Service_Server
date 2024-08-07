280b682cf70da37db41833c4102cbd228629f456
fojletuhin <fojletuhin@gmail.com>
2024-08-08 02:43:57 +0600

    request for cash in functionality done

--- index.js
+++ index.js
@@ -270,6 +270,7 @@ async function run() {
                 await usersCollection.updateOne(receiverQuery, updateReceiver);
                 const typeOfTransection = 'Send money'
                 const transection = {
+                    status: 'Done',
                     money,
                     sender,
                     receiver,
@@ -337,6 +338,7 @@ async function run() {
                 await usersCollection.updateOne(receiverQuery, updateReceiver);
                 const typeOfTransection = 'Cash out'
                 const transection = {
+                    status: 'Done',
                     money,
                     sender,
                     receiver,
@@ -356,6 +358,84 @@ async function run() {
         });
 
 
+
+        //request agent for cash in
+        app.post('/cashIn', async (req, res) => {
+            let { receiver, agent, money, password } = req.body;
+
+            try {
+                // Find the receiver
+                const receiverQuery = { number: receiver };
+                const receiverResult = await usersCollection.findOne(receiverQuery);
+
+                if (!receiverResult) {
+                    return res.status(400).json({ message: 'receiver not found' });
+                }
+
+                // Check if the password matches
+                const isMatch = await bcrypt.compare(password, receiverResult.pin);
+                if (!isMatch) {
+                    return res.status(400).json({ message: 'Wrong password' });
+                }
+
+                // Find the agent
+                const agentQuery = { number: agent, role: 'Agent' };
+                const agentResult = await usersCollection.findOne(agentQuery);
+
+                if (!agentResult) {
+                    return res.status(400).json({ message: 'Agent not found' });
+                }
+
+
+                const requestForCashIn = {
+                    status: 'pending',
+                    typeOfTransection: 'cash in',
+                    money,
+                    receiver,
+                    agent
+                }
+
+                const result = await transectionsCollection.insertOne(requestForCashIn);
+                res.send(result);
+
+            } catch (err) {
+                console.error('Error during cash out:', err);
+                res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+        app.patch('/ConfirmCashIn', async (req, res) => {
+
+
+            // Update the sender's balance
+            // const updateSender = {
+            //     $set: {
+            //         balance: senderResult.balance - money
+            //     }
+            // };
+            // await usersCollection.updateOne(senderQuery, updateSender);
+
+
+            // // Update the receiver's balance
+            // const updateReceiver = {
+            //     $set: {
+            //         balance: receiverResult.balance + money
+            //     }
+            // };
+            // await usersCollection.updateOne(receiverQuery, updateReceiver);
+            // const typeOfTransection = 'Cash out'
+            // const transection = {
+            //     money,
+            //     sender,
+            //     receiver,
+            //     typeOfTransection
+
+            // }
+            // transectionsCollection.insertOne(transection);
+
+            // // Send a success response
+            // res.status(200).json({ message: 'Money sent successfully' });
+        })
 
 
 
